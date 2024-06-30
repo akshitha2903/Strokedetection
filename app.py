0c679cdf293d72425fdf913fc4b2157bd816c26e
Akshitha R <r.akshitha2903@gmail.com>
2024-06-30 20:04:30 +0530

    modified app.py and home.html

--- app.py
+++ app.py
@@ -1,24 +1,26 @@
-import pickle
-from flask import Flask, request, jsonify, render_template
 import numpy as np
+from flask import Flask, request, jsonify, render_template
 from tensorflow.keras.preprocessing import image
-from tensorflow.keras.models import load_model
-
+import tensorflow as tf
+import os
+import io
 app = Flask(__name__)
 
 # Load your trained model
-densemodel = pickle.load(open('densemodel.pkl', 'rb'))
+model_path = 'densemodel.h5'  # Adjusted to .h5 format
+model = tf.keras.models.load_model(model_path)
 
-# Function to preprocess images
-def preprocess_image(img):
-    img = image.img_to_array(img)
-    img = np.expand_dims(img, axis=0)
-    img /= 255.0
-    return img
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+def preprocess_image(img):
+    img = img.resize((224, 224))  # Resize image to match model's expected sizing
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+    img /= 255.0
+    return img
 
 @app.route('/predict_api', methods=['POST'])
 def predict_api():
@@ -31,13 +33,13 @@ def predict_api():
         return jsonify({'error': 'No selected file'})
 
     try:
-        # Read image file string data
-        img = image.load_img(file, target_size=(224, 224))
+        # Read image file data
+        img = image.load_img(io.BytesIO(file.read()), target_size=(224, 224))
         img_array = preprocess_image(img)
 
         # Predict using your model
-        prediction = densemodel.predict(img_array)
-        classification = 'Stroke' if prediction > 0.5 else 'Normal'
+        prediction = model.predict(img_array)
+        classification = 'Stroke' if prediction < 0.5 else 'Normal'
 
         # Format prediction response
         result = {
@@ -49,6 +51,8 @@ def predict_api():
 
     except Exception as e:
         return jsonify({'error': str(e)})
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
